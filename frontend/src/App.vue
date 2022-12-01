d2b70205b7ad8144d301f65a0eebd02ae843c40b
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-12-01 22:33:15 +0900

    LMS APIへの接続テスト

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+import LmsApi from "@/modules/LmsApi"
+
+const apiTest = async () => {
+  const response = await LmsApi.post("/LmsLogin")
+  console.log(response)
+}
 </script>
 
-<template>
+<template lang="pug">
+button(@click="apiTest") Test
 </template>
 
-<style scoped>
+<style lang="sass" scoped>
 </style>
---
+++ frontend/src/modules/LmsApi.ts
@@ -0,0 +1,5 @@
+import axios from "axios"
+
+export default axios.create({
+  baseURL: "https://lluwjia5je.execute-api.ap-northeast-3.amazonaws.com/prod",
+})
